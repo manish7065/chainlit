2cf96f8d6fd99a05d9424f35edd142d6dc3ff17c
manish <manumi296@gmail.com>
2023-07-11 15:19:42 +0530

    working

--- src/llm.py
+++ src/llm.py
@@ -9,7 +9,7 @@ def get_gpt_response(messages,model="gpt-3.5-turbo",temperature=0):
         response=openai.ChatCompletion.create(
             model=model,
             temperature=temperature,
-            message=messages
+            messages=messages
         )
         return response.choices[0].message["content"]
         
